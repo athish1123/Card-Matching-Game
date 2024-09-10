ac3ef73666f50a1c5fd854d1e635b1b4903da07f
amita004 <amita004@d.umn.edu>
2024-09-09 19:22:16 -0500

    Added Grid and Assigned it a value

--- Game.py
+++ Game.py
@@ -1,12 +1,47 @@
 import itertools
-# import random
+import random
 
 class Cards:
+    def __init__(self):
+            self.Deck = self.CardSetup()
+
     def CardSetup(fun):
         Suit = ["♡", "♤", "♢", "♧"]
-        Value = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+        Value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         Deck = list(itertools.product(Value, Suit))
-        print(Deck)
+        random.shuffle(Deck)
+        return Deck
+
+    def PopDeck(self):
+        if self.Deck:
+            return self.Deck.pop()
+    
+
+class Grid:
+    def __init__(self, card_deck):
+        self.card_deck = card_deck
+
+    def GridSetup(self):
+        rows = [1, 2, 3, 4]
+        columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
+        Dock = []
+        for row in rows:
+            for col in columns:
+                RC = ("%s%d" % (col, row)) 
+                Value = self.card_deck.PopDeck()
+                Dock.append((RC, Value))
+                print(("%s%d" % (col, row)), end=" ")
+            print()
 
 Cardds = Cards()
 Cardds.CardSetup()
+
+GridS = Grid(Cardds)
+GridS.GridSetup()
+
+# x = 0
+# while(x < 3):
+#     print("*", end=" ")
+#     x = x + 1
+
+    
